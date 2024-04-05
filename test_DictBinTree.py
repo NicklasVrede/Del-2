fb04890305298d5c66f3ae31e0a2441c4debc02b
Nicklas Vrede <32699906+NicklasVrede@users.noreply.github.com>
2024-04-05 13:13:48 +0200

    update

--- test_DictBinTree.py
+++ test_DictBinTree.py
@@ -3,10 +3,10 @@ import DictBinTree as dbt
 
 class TestDictBinTree(TestCase):
     def setUp(self):
-        self.T = dbt.createEmptyDict()
-        dbt.insert(self.T, 5)
-        dbt.insert(self.T, 3)
-        dbt.insert(self.T, 7)
+        self.T = dbt.DictBinTree()
+        self.T.insert(self.T, 5)
+        self.T.insert(self.T, 3)
+        self.T.insert(self.T, 7)
 
     def test_insert(self):
         self.assertEqual(self.T.rod.nøgle, 5)  # Output: 5
@@ -14,14 +14,14 @@ class TestDictBinTree(TestCase):
         self.assertEqual(self.T.rod.højre.nøgle, 7)
 
     def test_search(self):
-        self.assertTrue(dbt.search(self.T, 5))
-        self.assertTrue(dbt.search(self.T, 3))
-        self.assertTrue(dbt.search(self.T, 7))
-        self.assertFalse(dbt.search(self.T, 8))
-        self.assertFalse(dbt.search(self.T, 2))
+        self.assertTrue(self.T.search(self.T, 5))
+        self.assertTrue(self.T.search(self.T, 3))
+        self.assertTrue(self.T.search(self.T, 7))
+        self.assertFalse(self.T.search(self.T, 8))
+        self.assertFalse(self.T.search(self.T, 2))
 
     def test_orderedTraversal(self):
-        self.assertEqual(dbt.orderedTraversal(self.T), [3, 5, 7])
+        self.assertEqual(self.T.orderedTraversal(self.T), [3, 5, 7])
 
 if __name__ == '__main__':
     from unittest import main
